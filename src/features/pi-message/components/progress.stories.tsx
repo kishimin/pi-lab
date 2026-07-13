771411b37dd06877c154d2899fdf6167415c3382
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-13 22:19:39 +0900

    feat: Add a display that shows “3.14 (pi to 50 decimal places)”...
    To create a standby screen

---
+++ src/features/pi-message/components/progress.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Progress } from "./progress";
+import { expect } from "storybook/test";
+
+const meta = {
+  component: Progress,
+} satisfies Meta<typeof Progress>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+/** Default state */
+export const Default: Story = {
+  play: async ({ step, canvas }) => {
+    await step("3.14(円周率50桁)...と表示される", () => {
+      expect(
+        canvas.getByRole("heading", {
+          name: "3.14159265358979323846264338327950288419716939937510...",
+        }),
+      );
+    });
+  },
+};
