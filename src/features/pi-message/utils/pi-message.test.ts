fd45673e379e76d14a62d9de86952feb4e9f9622
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-13 23:21:57 +0900

    feat: Add a message that appears when the value is 30 or more but less than 40
    To ensure that the message is displayed conditionally

--- src/features/pi-message/utils/pi-message.test.ts
+++ src/features/pi-message/utils/pi-message.test.ts
@@ -1,7 +1,16 @@
+import { vi } from "vitest";
 import { generateMessageResult } from "./pi-message";
 
 test("1以上30未満の時該当のメッセージが表示される", () => {
+  vi.spyOn(Math, "random").mockReturnValueOnce(0.1);
   const result = generateMessageResult();
 
   expect(result).toBe("3.14まで頑張りましたが、伝わりませんでした");
+});
+
+test("30以上40未満の時該当のメッセージが表示される", () => {
+  vi.spyOn(Math, "random").mockReturnValueOnce(0.3);
+  const result = generateMessageResult();
+
+  expect(result).toBe("3で伝えるには、雑すぎました");
 });
--- src/features/pi-message/utils/pi-message.ts
+++ src/features/pi-message/utils/pi-message.ts
@@ -1,3 +1,12 @@
 export const generateMessageResult = (): string => {
-  return "3.14まで頑張りましたが、伝わりませんでした";
+  const result = Math.floor(Math.random() * 100) + 1;
+  if (result < 30) {
+    return "3.14まで頑張りましたが、伝わりませんでした";
+  }
+
+  if (result < 40) {
+    return "3で伝えるには、雑すぎました";
+  }
+
+  return "";
 };
