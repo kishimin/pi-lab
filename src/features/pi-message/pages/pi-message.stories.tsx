67d5b8335c38f280c310eb9c1d8450f9f3164b15
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-14 06:57:01 +0900

    feat: The “Conveying with π” heading is displayed
    To display the page title

---
+++ src/features/pi-message/pages/pi-message.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { PiMessagePage } from "./pi-message";
+import { expect } from "storybook/test";
+
+const meta = {
+  component: PiMessagePage,
+} satisfies Meta<typeof PiMessagePage>;
+
+export default meta;
+type Story = StoryObj<typeof PiMessagePage>;
+
+/** Default state */
+export const Default: Story = {
+  play: async ({ step, canvas }) => {
+    await step("πで伝えるのheadingが表示される", () => {
+      expect(canvas.getByRole("heading", { name: "πで伝える" })).toBeVisible();
+    });
+  },
+};
---
+++ src/features/pi-message/pages/pi-message.tsx
@@ -0,0 +1,7 @@
+export const PiMessagePage = () => {
+  return (
+    <>
+      <h1>{"πで伝える"}</h1>
+    </>
+  );
+};
