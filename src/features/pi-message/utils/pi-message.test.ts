43b603d4bd2172c0fd12d763ed309954f885ab4d
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-13 23:34:38 +0900

    Add a function to display randomly generated messages
    To allow messages to be displayed in different branches

--- src/features/pi-message/utils/pi-message.test.ts
+++ src/features/pi-message/utils/pi-message.test.ts
@@ -1,19 +1,49 @@
 import { vi } from "vitest";
 import { generateMessageResult } from "./pi-message";
 
-const spyRandom = (returnValue: number) =>
-  vi.spyOn(Math, "random").mockReturnValue(returnValue);
+describe("generateMessageResult", () => {
+  const spyRandom = (returnValue: number) =>
+    vi.spyOn(Math, "random").mockReturnValue(returnValue);
 
-test("1以上30未満の時該当のメッセージが表示される", () => {
-  spyRandom(0.1);
-  const result = generateMessageResult();
+  test("1の時3.14まで頑張りましたが、伝わりませんでしたと表示される", () => {
+    spyRandom(0.1);
+    const result = generateMessageResult();
 
-  expect(result).toBe("3.14まで頑張りましたが、伝わりませんでした");
-});
+    expect(result).toBe("3.14まで頑張りましたが、伝わりませんでした");
+  });
 
-test("30以上40未満の時該当のメッセージが表示される", () => {
-  spyRandom(0.3);
-  const result = generateMessageResult();
+  test("30の時3で伝えるには、雑すぎましたと表示される", () => {
+    spyRandom(0.3);
+    const result = generateMessageResult();
 
-  expect(result).toBe("3で伝えるには、雑すぎました");
+    expect(result).toBe("3で伝えるには、雑すぎました");
+  });
+
+  test("40の時πでは伝わりませんと表示される", () => {
+    spyRandom(0.4);
+    const result = generateMessageResult();
+
+    expect(result).toBe("πでは伝わりません");
+  });
+
+  test("80の時τで伝えるのは良いかもと表示される", () => {
+    spyRandom(0.8);
+    const result = generateMessageResult();
+
+    expect(result).toBe("τで伝えるのは良いかも");
+  });
+
+  test("90の時あなたのメッセージは伝わりませんと表示される", () => {
+    spyRandom(0.9);
+    const result = generateMessageResult();
+
+    expect(result).toBe("このメッセージは伝わりません");
+  });
+
+  test("95の時eでは伝わるのかな？と表示される", () => {
+    spyRandom(0.95);
+    const result = generateMessageResult();
+
+    expect(result).toBe("eでは伝わるのかな？");
+  });
 });
--- src/features/pi-message/utils/pi-message.ts
+++ src/features/pi-message/utils/pi-message.ts
@@ -1,5 +1,6 @@
 export const generateMessageResult = (): string => {
   const result = Math.floor(Math.random() * 100) + 1;
+
   if (result < 30) {
     return "3.14まで頑張りましたが、伝わりませんでした";
   }
@@ -8,5 +9,17 @@ export const generateMessageResult = (): string => {
     return "3で伝えるには、雑すぎました";
   }
 
-  return "";
+  if (result < 80) {
+    return "πでは伝わりません";
+  }
+
+  if (result < 90) {
+    return "τで伝えるのは良いかも";
+  }
+
+  if (result < 95) {
+    return "このメッセージは伝わりません";
+  }
+
+  return "eでは伝わるのかな？";
 };
