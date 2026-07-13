93d5a13e5cb7bfac408e4a80761cfb0e956b10e7
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-13 23:25:03 +0900

    refactor: Changed to use  to make the mocking process DRY

--- src/features/pi-message/utils/pi-message.test.ts
+++ src/features/pi-message/utils/pi-message.test.ts
@@ -1,15 +1,18 @@
 import { vi } from "vitest";
 import { generateMessageResult } from "./pi-message";
 
+const spyRandom = (returnValue: number) =>
+  vi.spyOn(Math, "random").mockReturnValue(returnValue);
+
 test("1以上30未満の時該当のメッセージが表示される", () => {
-  vi.spyOn(Math, "random").mockReturnValueOnce(0.1);
+  spyRandom(0.1);
   const result = generateMessageResult();
 
   expect(result).toBe("3.14まで頑張りましたが、伝わりませんでした");
 });
 
 test("30以上40未満の時該当のメッセージが表示される", () => {
-  vi.spyOn(Math, "random").mockReturnValueOnce(0.3);
+  spyRandom(0.3);
   const result = generateMessageResult();
 
   expect(result).toBe("3で伝えるには、雑すぎました");
