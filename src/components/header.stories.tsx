3771fd850c353193e65f3e04cf8912e0e9ac82ac
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-22 06:42:30 +0900

    feat: Create a header
    To ensure the top section is visible

--- src/components/header.stories.tsx
+++ src/components/header.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { Header } from "./header";
-import { expect } from "storybook/test";
+import { expect, within } from "storybook/test";
 
 const meta = {
   component: Header,
@@ -12,10 +12,22 @@ type Story = StoryObj<typeof Header>;
 /** Default state */
 export const Default: Story = {
   play: async ({ step, canvas }) => {
+    const header = canvas.getByRole("banner");
+    await step("ヘッダーが表示される", async () => {
+      await expect(header).toBeVisible();
+    });
+
     await step("割り切れない研究所のheadingが表示される", async () => {
       await expect(
-        canvas.getByRole("heading", { name: "割り切れない研究所" }),
+        within(header).getByRole("heading", { name: "割り切れない研究所" }),
       ).toBeVisible();
+    });
+
+    await step("ロゴ画像が表示される", async () => {
+      const image = within(header).getByAltText("pi-lab-logo-image");
+
+      await expect(image).toBeInTheDocument();
+      await expect(image).toHaveAttribute("src", "/src/assets/pi-lab-logo.png");
     });
   },
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,10 @@
+import logoImage from "../assets/pi-lab-logo.png";
+
 export const Header = () => {
   return (
-    <>
+    <header>
       <h2>{"割り切れない研究所"}</h2>
-    </>
+      <img src={logoImage} alt={"pi-lab-logo-image"} />
+    </header>
   );
 };
