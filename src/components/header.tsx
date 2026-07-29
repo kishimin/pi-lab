004c78ffbaaf4daa9ec69a369ca9a25cb6b479ef
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-29 22:04:13 +0900

    feat: I tweaked the header design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,12 +2,12 @@ import logoImage from "../assets/pi-lab-logo.png";
 
 export const Header = () => {
   return (
-    <header className={"flex items-center"}>
+    <header className={"flex items-center bg-[#7cc7e8]"}>
       <h2>{"割り切れない研究所"}</h2>
       <img
         src={logoImage}
         alt={"pi-lab-logo-image"}
-        className={"w-1/5 lg:w-1/10"}
+        className={"w-1/5 md:w-1/10"}
       />
     </header>
   );
