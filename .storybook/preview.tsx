7c2b60cf661b3ac35cb0d86127b7a08ecc37be25
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-29 21:50:51 +0900

    feat: I reduced the size of the header image

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
+/// <reference types="vite/client" />
 import type { Preview } from "@storybook/react-vite";
 import { MemoryRouter } from "react-router";
+import "../src/index.css";
 
 const preview: Preview = {
   parameters: {
