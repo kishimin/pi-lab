9e8e78bc750b62f8c0b4f5e62d140ed3201f567a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-07-29 21:46:29 +0900

    feat: apply mobile header logo width by default

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,9 +2,13 @@ import logoImage from "../assets/pi-lab-logo.png";
 
 export const Header = () => {
   return (
-    <header>
+    <header className={"flex items-center"}>
       <h2>{"割り切れない研究所"}</h2>
-      <img src={logoImage} alt={"pi-lab-logo-image"} />
+      <img
+        src={logoImage}
+        alt={"pi-lab-logo-image"}
+        className={"w-1/5 lg:w-1/10"}
+      />
     </header>
   );
 };
