4c48400c3a5ec77abe000d9de4bab985642f396a
Kazuma-Ishimine <r8a1wu2@gmail.com>
2026-08-03 06:37:02 +0900

    feat: center the index navigation
    Help users find the primary action immediately by giving it visual focus within the available content area.

--- src/features/pi-loop/pages/pi-loop.tsx
+++ src/features/pi-loop/pages/pi-loop.tsx
@@ -4,7 +4,9 @@ import { Links } from "../components/links";
 export const PiLoopPage = () => {
   return (
     <Layout title={undefined}>
-      <Links />
+      <main className={"flex flex-1 items-center justify-center"}>
+        <Links />
+      </main>
     </Layout>
   );
 };
